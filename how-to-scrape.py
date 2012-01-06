4f117533489e8c197151d4eef6797f484f95f3fb
Thomas Levine <thomas.levine@gmail.com>
2012-01-06 15:21:35 -0500

    getcode function to load scraper code

--- how-to-scrape.py
+++ how-to-scrape.py
@@ -1,1 +1,16 @@
-oue
+from json import loads,dumps
+from urllib2 import urlopen
+
+def url(scraper_name):
+  return "https://api.scraperwiki.com/api/1.0/scraper/getinfo?format=jsondict&name="+ \
+  scraper_name+ \
+  "&version=-1&quietfields=runevents%7Cdatasummary%7Cuserroles%7Chistory"
+
+def getcode(scraper_name):
+  return loads(urlopen(url(scraper_name)).read())[0]['code']
+
+def demo():
+  print getcode('rifidec')
+
+if __name__ == "__main__":
+  demo()
